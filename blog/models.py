d9ad6f16cf73e2a085557d0870491140c21c5054
Yerassil <yerassil@gmail.com>
2020-04-30 16:54:50 +0600

    Add post_list and post_detail views, add templates

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -25,7 +32,9 @@ class Post(models.Model):
     objects = models.Manager()
     published = PublishedManager()
 
-
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super().get_queryset().filter(status='published')
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
